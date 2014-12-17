ac2f9e6228cf91ab70f04b3f2d1ac6a41fe390d3
Henrique Carvalho Silva <henrique.cvlhs@gmail.com>
2014-12-17 20:11:17 -0200

    [KWCP-4]
    - Funcionalidade de comunicação básica via bluetooth

--- kitwearablecpfirmware.ino
+++ kitwearablecpfirmware.ino
@@ -6,6 +6,9 @@
  * Constants and pins                                           *
  ****************************************************************/
 
+const char BLUETOOTH_DEVICE_NAME[] = "wearable";
+const char BLUETOOTH_DEVICE_PIN[5] = "1234";
+
 /****************************************************************
  * Function Prototypes                                          *
  ****************************************************************/
@@ -17,6 +20,34 @@
 /****************************************************************
  * Arduino main functions                                       *
  ****************************************************************/
+
+void setup()
+{   
+    Serial.begin(9600);
+    
+    /* Setup HC-06 bluetooth module */
+    Serial1.begin(9600);  /* HC-06 default */
+    char command[64];
+    sprintf(command, "AT+NAME%s\n\r", BLUETOOTH_DEVICE_NAME);
+    Serial1.print(command);
+    delay(1100);
+    sprintf(command, "AT+PIN%s\n\r", BLUETOOTH_DEVICE_PIN);
+    Serial1.print(command);
+}
+
+void loop()
+{
+    while (Serial1.available())
+    {
+       Serial.write((char) Serial1.read()); 
+    }
+
+    while (Serial.available())
+    {
+        Serial1.write(Serial.read());
+    }
+    delay(100);
+}
 
 /****************************************************************
  * Function Definitions                                         *
