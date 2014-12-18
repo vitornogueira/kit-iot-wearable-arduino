8d4ec2cc01be63a5c83d51dd069ea9a8bb9b9154
Henrique Carvalho Silva <henrique.cvlhs@gmail.com>
2014-12-18 16:24:56 -0200

    [KWCP-5]
    - Implementação da máquina de estados que recebe os comandos via
      bluetooth

--- kitwearablecpfirmware.ino
+++ kitwearablecpfirmware.ino
@@ -13,6 +13,8 @@
  ****************************************************************/
 
 #define DEBUG
+#define DEBUG_DEVICE_SM
+#define DEBUG_PROTOCOL_SM
 
 const char BLUETOOTH_DEVICE_NAME[] = "wearable";
 const char BLUETOOTH_DEVICE_PIN[5] = "1234";
@@ -27,13 +29,15 @@ enum Device
     LED
 };
 
-/* Device ID code for the protocol */
+/* Device ID code for the bluetooth protocol */
 const char DEVICE_CODE[DEVICE_NUMBER][3] = 
 {
     "LE"  /* Led */
 };
+#define PROTOCOL_COMMAND_LENGHT 5
+#define PROTOCOL_DEVICE_CODE_LENGTH 2
 
-Pstate SM_STATE[DEVICE_NUMBER + 1] = 
+Pstate DEVICE_STATE[DEVICE_NUMBER + 1] = 
 {
     ledState,
     waitForCommandState  /* Must always be last */
@@ -47,8 +51,15 @@ Pstate SM_STATE[DEVICE_NUMBER + 1] =
  * Global variables                                             *
  ****************************************************************/
 
-SM stateMachine(waitForCommandState);
-int bluetoothCommandArgument;
+SM deviceStateMachine(waitForCommandState);
+SM protocolStateMachine(sleepState);
+
+bool protocolCommandIsReady = false;
+bool readyToReceiveProtocolCommand = true;
+
+char protocolCommandString[PROTOCOL_COMMAND_LENGHT + 1];
+char* protocolCommand;
+int protocolCommandArgument;
 
 /****************************************************************
  * Arduino main functions                                       *
@@ -62,85 +73,73 @@ void setup()
     
     /* Setup HC-06 bluetooth module */
     Serial1.begin(9600);  /* HC-06 default */
-    char command[64];
-    sprintf(command, "AT+NAME%s\n\r", BLUETOOTH_DEVICE_NAME);
-    Serial1.print(command);
+    char atCommand[64];
+    sprintf(atCommand, "AT+NAME%s\n\r", BLUETOOTH_DEVICE_NAME);
+    Serial1.print(atCommand);
     delay(1100);
-    sprintf(command, "AT+PIN%s\n\r", BLUETOOTH_DEVICE_PIN);
-    Serial1.print(command);
+    sprintf(atCommand, "AT+PIN%s\n\r", BLUETOOTH_DEVICE_PIN);
+    Serial1.print(atCommand);
 
     pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    EXEC(stateMachine);
+    EXEC(deviceStateMachine);
+    EXEC(protocolStateMachine);
 }
 
 /****************************************************************
- * Function Definitions                                         *
+ * Device state machine functions                               *
  ****************************************************************/
 
 State waitForCommandState()
 {
-    char commandString[7];
-    char* command = commandString;
-    char commandCode[3];
-    char receivedChar;    
-    int  receivedCharIndex = 0;
+    char deviceCode[PROTOCOL_DEVICE_CODE_LENGTH + 1];
+    
+    deviceStateMachine.Set(waitForCommandState);
 
-    stateMachine.Set(waitForCommandState);
+    if (!protocolCommandIsReady) return;
 
-    /* Get command from serial port */
-    while (Serial.available())
-    {
-        receivedChar = (char) Serial.read();
-        if (receivedChar == '\n' || receivedChar == '\n' || receivedCharIndex > 5)
-        {
-            receivedCharIndex++;
-            continue;
-        }
-
-        command[receivedCharIndex] = receivedChar;
-        receivedCharIndex++;
-    }
-
-    (receivedCharIndex > 5) ? command[6] = '\0' : command[receivedChar] = '\0';
-
-    if (strlen(command) == 0) return;
-
-    /* Parse command */
-    if (command[0] != '#') return;
+    /* Parse device code */
+    for (int i = 0; i < PROTOCOL_DEVICE_CODE_LENGTH; i++) deviceCode[i] = protocolCommand[i];
+    deviceCode[PROTOCOL_DEVICE_CODE_LENGTH] = '\0';
 
     #ifdef DEBUG
-        Serial.print("Debug: Received command: ");
-        Serial.println(commandString);
+        Serial.print("Debug: Parsed Command Device Code \"");
+        Serial.print(deviceCode);
+        Serial.println("\"");
     #endif
-    
-    command++;
-    for (int i = 0; i < 2; i++) commandCode[i] = command[i];
-    commandCode[2] = '\0';
 
-    command += 2;
-    bluetoothCommandArgument = atoi(command);
-    
     for (int i = 0; i < DEVICE_NUMBER; i++)
     {
-        if (strcmp(commandCode, DEVICE_CODE[i]) == 0)
+        if (strcmp(deviceCode, DEVICE_CODE[i]) == 0)
         {
-            stateMachine.Set(SM_STATE[i]);
-            return;
+            deviceStateMachine.Set(DEVICE_STATE[i]);
         }
     }
-    
+
+    /* Parse command argument */
+    protocolCommand += PROTOCOL_DEVICE_CODE_LENGTH;
+    protocolCommandArgument = atoi(protocolCommand);    
+
+    #ifdef DEBUG
+        Serial.print("Debug: Parsed Command Argument \"");
+        Serial.print(protocolCommandArgument);
+        Serial.println("\"");
+    #endif
+
+    readyToReceiveProtocolCommand = true;
+    return;    
 }
 
 State ledState()
 {
-    #ifdef DEBUG
-        Serial.println("Debug: LED state");
+    #ifdef DEBUG_DEVICE_SM
+        Serial.println("Debug Device State Machine: LED state");
     #endif
-    if (bluetoothCommandArgument)
+
+    if (protocolCommandArgument)
     {
         digitalWrite(LED_PIN, HIGH);
     }
@@ -149,5 +148,91 @@ State ledState()
         digitalWrite(LED_PIN, LOW);
     }
 
-    stateMachine.Set(waitForCommandState);
+    deviceStateMachine.Set(waitForCommandState);
+}
+
+/****************************************************************
+ * Protocol state machine functions                             *
+ ****************************************************************/
+
+State sleepState()
+{
+    if (readyToReceiveProtocolCommand)
+    {
+        protocolStateMachine.Set(waitForStartState);
+        protocolCommandIsReady = false;
+    }
+    else
+    {
+        protocolStateMachine.Set(sleepState);
+    }
+}
+
+State waitForStartState()
+{
+    if (!Serial1.available())
+    {
+        protocolStateMachine.Set(waitForStartState);
+    }
+    else
+    {
+        char receivedChar = Serial1.read();
+        if (receivedChar == '#')
+        {
+            /* Start of protocol command detetcted */
+            #ifdef DEBUG_PROTOCOL_SM
+                Serial.println("Debug Protocol State Machine: Wait For Start State => '#'");
+            #endif
+
+            strcpy(protocolCommandString, "");
+            protocolCommand = protocolCommandString;
+            protocolStateMachine.Set(receiveCharState);
+        }
+        else
+        {
+            protocolStateMachine.Set(waitForStartState);
+        }
+    }
+}
+
+State receiveCharState()
+{
+    if (!Serial1.available())
+    {
+        protocolStateMachine.Set(receiveCharState);
+    }
+    else
+    {
+        char receivedChar = Serial1.read();
+
+        #ifdef DEBUG_PROTOCOL_SM
+            Serial.print("Debug Protocol State Machine: Receive Char State => '");
+            Serial.print(receivedChar);
+            Serial.println("'");
+        #endif
+
+        if (receivedChar == '\n' || receivedChar == '\r' || strlen(protocolCommand) > 5)
+        {
+            /* End of protocol command detected */
+            *protocolCommand = '\0';
+            protocolCommandIsReady = true;
+            readyToReceiveProtocolCommand = false;
+            protocolCommand = protocolCommandString;
+            protocolStateMachine.Set(sleepState);
+            
+            #ifdef DEBUG
+                Serial.print("Debug: Received command \"#");
+                Serial.print(protocolCommand);
+                Serial.println("\"");
+            #endif
+        }
+        else            
+        {
+            /* Keep receiving chars unitl the end is detected */
+            *protocolCommand = receivedChar;
+            *(protocolCommand + 1) = '\0';
+            protocolCommand++;
+            protocolStateMachine.Set(receiveCharState);
+        }
+    }
 }
